94897a2570851b372c0cef800bc511c659a92ca1
Hamid Peywasti <hamid.peywasti@gmail.com>
2025-11-16 18:44:32 +0330

    create footer component

---
+++ app/components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return(
+    <footer className="bg-blue-950 text-white flex justify-center p-2">
+      <p>&copy; ۱۴۰۴، کلیه حقوق این وب‌سایت محفوظ می‌باشد.</p>
+    </footer>
+  )
+}
