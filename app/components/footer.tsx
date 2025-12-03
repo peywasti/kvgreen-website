bb086f667d2f1f350335bbf5a35d7109176adb5c
Hamid Peywasti <hamid.peywasti@gmail.com>
2025-12-03 19:56:52 +0330

    improve

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return(
-    <footer className="bg-blue-950 text-white flex justify-center p-2">
+    <footer className="dark:bg-blue-950 dark:text-white flex justify-center p-2">
       <p>&copy; ۱۴۰۴، کلیه حقوق این وب‌سایت محفوظ می‌باشد.</p>
     </footer>
   )
