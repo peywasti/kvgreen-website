e8e0b425ad8b47cdf6166b85a58a67dffa6fe593
Hamid Peywasti <hamid.peywasti@gmail.com>
2025-12-06 11:05:13 +0330

    add logo

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-200 font-sans dark:bg-black">
       <main className="flex flex-col justify-center items-center py-24 px-32 rounded-sm bg-white dark:bg-gray-900 sm:items-start">
-        <Image
-        src="/kvgreen-logo.svg"
-        width={100}
-        height={100}
-        alt="KV-Green Logo"
-        className="mx-auto"
-      />        
+                
         <h1 className="text-lg font-bold self-center">کی‌وی‌گرین</h1>
         <p>وب‌سایت در حال طراحی می‌باشد.</p>
       </main>
