2cd3e097bc454dd7baa9adfc6e686d6a8f6e9924
Hamid Peywasti <hamid.peywasti@gmail.com>
2025-12-06 18:13:48 +0330

    simplify footer template

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,7 +1,9 @@
 export default function Footer() {
-  return(
-    <footer className="dark:bg-blue-950 dark:text-white flex justify-center p-2">
-      <p>&copy; ۱۴۰۴، کلیه حقوق این وب‌سایت محفوظ می‌باشد.</p>
+  return (
+    <footer className="w-full border-t bg-gray-100 text-gray-700">
+      <div className="max-w-6xl mx-auto p-4 flex items-center justify-center text-sm">
+        <p>&copy; ۱۴۰۴ — کلیه حقوق این وب‌سایت محفوظ است.</p>
+      </div>
     </footer>
-  )
+  );
 }
