7c28d370c014f90587519228429afe7a6fd8da76
Hamid Peywasti <hamid.peywasti@gmail.com>
2025-12-07 17:19:34 +0330

    move component folder to root folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
-import Header from "./components/header";
-import Footer from "./components/footer";
+import Header from "../components/header";
+import Footer from "../components/footer";
 import { Vazirmatn } from "next/font/google";
 import "./globals.css";
 
